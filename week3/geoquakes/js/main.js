8e1b7f5e16870a799b10611c728e839dc3ada11a
Tay J Solis <tayjsolis@gmail.com>
2018-09-26 15:26:57 -0700

    Completed geoquakes lab

--- week3/geoquakes/js/main.js
+++ week3/geoquakes/js/main.js
@@ -1,1 +1,78 @@
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var weekly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
+const today = new Date();
+
+let map;
+
+const quakeIcon = {
+  url: './img/earthquakephoto.png',
+  scaledSize: new google.maps.Size(40, 40)
+}
+
+function addMarker(location){
+  let marker = new google.maps.Marker({
+    position:location,
+    map: map,
+    icon: quakeIcon
+  });
+}
+/*
+Takes in a past date and converts it to a string:
+if < 24 hours ago, returns x hours ago
+if > 24 hours but < 2 days, returns 1 day ago
+if >2 days returns x days ago
+*/
+function timeAgo(past){
+  let time = Math.floor((today - past)/(1000* 60 * 60));
+  if (time < 24){
+    return`${time} hours ago`;
+  } else{
+    time = Math.floor(time/24);
+    if (time === 1) {
+      return `${time} day ago`;
+    }else{
+      return `${time} days ago`;
+    }
+  }
+}
+
+$.ajax({
+  method: "GET",
+  url: weekly_quakes_endpoint,
+  success: function(response) {
+    for (let i = 0; i < response.features.length; i++) {
+      let quake = response.features[i];
+      console.log(quake);
+      let mag = quake.properties.mag;
+      let place = quake.properties.place
+      let quakeDate = new Date(quake.properties.time);
+      let timeStr = timeAgo(quakeDate);
+
+      $('#info').append(`<p><span class="mag">M ${mag}</span> // ${place} // ${timeStr}</p>`);
+
+      addMarker({
+        lng: quake.geometry.coordinates[0],
+        lat: quake.geometry.coordinates[1]
+      });
+
+    }
+  }
+});
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 37.78,
+      lng: -122.44
+    },
+    zoom: 4
+  });
+  let marker = new google.maps.Marker({
+    position:{
+      lat: 37.78,
+      lng: -122.44},
+    map: map,
+  });
+}
+
+initMap();
