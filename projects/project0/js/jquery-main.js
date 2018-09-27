b3ea59ce7083d6fd43e93e3db061fc22e0dea168
Tay J Solis <tayjsolis@gmail.com>
2018-09-27 15:01:17 -0700

    Completed navbar functionality

--- projects/project0/js/jquery-main.js
+++ projects/project0/js/jquery-main.js
@@ -2,14 +2,14 @@
 
 
 /* Each slide in the slideshow consists of a header image, a title, an description, and a link to the proper page.*/
-class Slide{
-  constructor(imgUrl, title, desc, link){
+class Slide {
+  constructor(imgUrl, title, desc, link) {
     this.imgUrl = imgUrl;
     this.title = title;
     this.desc = desc;
     this.link = link;
   }
-  createSlide(){
+  createSlide() {
     let elm = `
                   <div class="label">
                   <h2>${this.title}</h2>
@@ -50,7 +50,7 @@ const wanderlustSlide = new Slide(
 );
 
 let slideshow = [wanderlustSlide, codeolingoSlide, syncSlide, christineSlide];
-  let place = 0;
+let place = 0;
 
 /*
 Receives an image URL from NASA's Astronomy Picture of the Day API and sets it as the header background
@@ -59,24 +59,24 @@ If the API is down, it leaves the default background up.
 $.ajax({
   method: 'GET',
   url: 'https://api.nasa.gov/planetary/apod?api_key=dcBZXp3YhRP5hfMxGNxJqCVEGHfPENe04CAdJste',
-  success: function(response){
+  success: function(response) {
     $('header').css("background-image", `url(${response.hdurl})`);
   },
-  error: function(){
+  error: function() {
     $('header').css("background-image"), 'url("../img/default-bg.jpg")'
   }
 });
 
-const setUpSlideshow = () =>{
-  for(let i = 0; i < slideshow.length; i++){
+const setUpSlideshow = () => {
+  for (let i = 0; i < slideshow.length; i++) {
     $('.place-dots').append(`<div class="dot"></div>`);
   }
   $('.place-dots').children().eq(0).addClass('selected');
   $('.slides').append(slideshow[0].createSlide());
 
-  $('.next').on('click', function(){
+  $('.next').on('click', function() {
     place++;
-    if(place < slideshow.length){
+    if (place < slideshow.length) {
       $('.place-dots').children().removeClass('selected');
       $('.place-dots').children().eq(place).addClass('selected');
       $('.slides').html(slideshow[place].createSlide());
@@ -87,9 +87,9 @@ const setUpSlideshow = () =>{
       $('.slides').html(slideshow[place].createSlide());
     }
   });
-  $('.prev').on('click', function(){
+  $('.prev').on('click', function() {
     place--;
-    if(place > 0){
+    if (place > 0) {
       $('.place-dots').children().removeClass('selected');
       $('.place-dots').children().eq(place).addClass('selected');
       $('.slides').html(slideshow[place].createSlide());
@@ -100,7 +100,34 @@ const setUpSlideshow = () =>{
       $('.slides').html(slideshow[place].createSlide());
     }
   });
+  $('.hamburger').on("click", function() {
+    $('.nav-links').slideToggle();
+    $('nav').toggleClass('shadow');
+  });
 
+  $("a[href*='#']").on('click', function(e) {
+    e.preventDefault();
+    var target = $(this.hash);
+    $('html, body').animate({
+      scrollTop: target.offset().top
+    }, 1000, function() {
+      target.focus();
+      if (target.is(":focus")) { // Checking if the target was focused
+        return false;
+      } else {
+        target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+        target.focus();
+      }
+    });
+  });
+
+  window.addEventListener("resize", function() {
+    if (window.matchMedia("(min-width: 480px)").matches) {
+      $('.nav-links').attr('style', 'display: flex;');
+    } else {
+      $('.nav-links').attr('style', 'display: none;')
+    }
+  });
 }
 
 setUpSlideshow();
