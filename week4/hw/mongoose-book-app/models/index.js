7e6da3f5dd250eb1888677187007814a62e54a82
Tay J Solis <tayjsolis@gmail.com>
2018-10-03 15:13:26 -0700

    Completed Week4 Day3 exercise and Mongoose Book App Sprint 2

--- week4/hw/mongoose-book-app/models/index.js
+++ week4/hw/mongoose-book-app/models/index.js
@@ -2,3 +2,4 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/book-app", {useNewUrlParser: true});
 
 module.exports.Book = require('./book');
+module.exports.Author = require('./author')
--- week4/hw/mongoose-book-app/server.js
+++ week4/hw/mongoose-book-app/server.js
@@ -17,21 +17,11 @@ var app = express();
 app.use(express.static('public'));
 
 // body parser config to accept our datatypes
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const db = require(`./models`);
-
-
-////////////////////
-//  DATA
-///////////////////
-
-var newBookUUID = 18;
-
-
-
-
-
 
 
 ////////////////////
@@ -40,77 +30,83 @@ var newBookUUID = 18;
 
 
 
-
 // define a root route: localhost:3000/
-app.get('/', function (req, res) {
-  res.sendFile('views/index.html' , { root : __dirname});
+app.get('/', function(req, res) {
+  res.sendFile('views/index.html', {
+    root: __dirname
+  });
 });
 
 // get all books
-app.get('/api/books', function (req, res) {
-  // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) {
-      console.log("index error: " + err);
-      res.sendStatus(500);
-    }
-    res.json(books);
+app.get('/api/books', (req, res) =>{
+  db.Book.find((err, allBooks) =>{
+    if (err) throw err;
+    res.json(allBooks);
   });
 });
 
+
 // get one book
-app.get('/api/books/:id', function (req, res) {
+app.get('/api/books/:id', function(req, res) {
   // find one book by its id
   console.log('books show', req.params);
   let bookId = req.params.id;
-  db.Book.findOne({_id: bookId}, (err, foundBook)=>{
-    if (err) return console.log(`Error at get one: ${err}`);
-    res.json(foundBook);
-  });
+  db.Book.findOne({ _id: bookId})
+          .populate('author')
+          .exec((err, foundBook) => {
+            if (err) throw err;
+            res.json(foundBook);
+          });
 });
 
 // create new book
-app.post('/api/books', function (req, res) {
-  // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  newBook.title = req.body.title;
-  newBook.author = req.body.author;
-  newBook.image = req.body.image;
-  newBook.releaseDate = req.body.releaseDate;
-
-  db.Book.create(newBook, (err, savedBook) =>{
-    if (err) return console.log(`Could not create new Book: ${err}`);
-    res.json(savedBook);
-  })
+app.post('/api/books', function(req, res) {
+  db.Book.create({title: req.body.title}, (err, newBook) =>{
+    if (err) throw err;
+    db.Author.findOne({name: req.body.author}, (err, foundAuthor) =>{
+      if(err) throw err;
+      newBook.author = foundAuthor;
+      newBook.save((err, savedBook) =>{
+        if(err) throw err;
+        console.log(`Created ${savedBook}`);
+        res.json(newBook);
+      });
+    });
+    console.log('Author not found');
+  });
 });
 
+
 // update book
-app.put('/api/books/:id', function(req,res){
-// get book id from url params (`req.params`)
+app.put('/api/books/:id', function(req, res) {
+  // get book id from url params (`req.params`)
   console.log('books update', req.params);
   var bookId = req.params.id;
   let updateBody = req.body;
 
-  db.Book.findOneAndUpdate({_id: bookId}, updateBody, {new:true}, (err, updatedBook) =>{
-    if(err) return console.log(`Could not update ${bookId}: ${err}`);
+  db.Book.findOneAndUpdate({
+    _id: bookId
+  }, updateBody, {
+    new: true
+  }, (err, updatedBook) => {
+    if (err) throw err;
     res.json(updatedBook);
   });
 });
 
 // delete book
-app.delete('/api/books/:id', function (req, res) {
+app.delete('/api/books/:id', function(req, res) {
   // get book id from url params (`req.params`)
   console.log('books delete', req.params);
   let bookId = req.params.id;
 
-  db.Book.findOneAndRemove({ _id: bookId }, (err, deletedBook) => {
-        res.json(deletedBook);
-    });
+  db.Book.findOneAndRemove({
+    _id: bookId
+  }, (err, deletedBook) => {
+    res.json(deletedBook);
+  });
 });
 
-
-
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log('Book app listening at http://localhost:3000/');
 });
