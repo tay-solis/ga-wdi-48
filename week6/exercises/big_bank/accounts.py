c8aa77f41a5416e4e56903f9e741b9ac9b2f6f6a
Tay J Solis <tayjsolis@gmail.com>
2018-10-15 17:01:15 -0700

    Day1 Week6 Homework Done

--- week6/exercises/big_bank/accounts.py
+++ week6/exercises/big_bank/accounts.py
@@ -4,6 +4,7 @@ class BankAccount:
     def __init__(self, type):
         self.balance = 0
         self.type = type
+        self.interest_rate = ".02"
         self.overdraft_fees = 0
     def withdraw(self, dollars):
         if self.overdraft_fees > 100:
@@ -18,17 +19,83 @@ class BankAccount:
         self.balance += dollars
         return self.balance
 
-rich_man = BankAccount("checking")
-rich_man.deposit(100000000)
+    def accumulate_interest(self):
+        self.balance *= 1.02
+        return self.balance
 
-def ballin():
-    rich_man.withdraw(random.randint(500000, 25000000))
+    def __compare__(self, other_account):
+        if self.balance == other_account.balance:
+            return 'equal'
+        elif self.balance > other_account.balance:
+            return 'greater'
+        elif self.balance >= other_account.balance:
+            return 'greater than or equal to'
+        elif self.balance < other_account.balance:
+            return 'less than'
+        else:
+            return 'less than or equal to'
 
-for i in range(1, 40):
-    if rich_man.overdraft_fees < 100:
-        ballin()
-        print("He has {} left".format(rich_man.balance))
-    else:
-        print("He's in ${} of debt after {} parties".format(rich_man.balance - 100, i))
-        break
-print("What a baller.")
+class ChildAccount(BankAccount):
+    def __init__(self):
+        super().__init__("child")
+        self.interest_rate = 0
+    def accumulate_interest(self):
+        self.balance += 10
+
+class OverdraftAccount(BankAccount):
+    def __init__(self):
+        super().__init__("overdraft")
+        self.overdraft_penalty = 40
+    def withdraw(self, dollars):
+        if dollars > self.balance:
+            self.balance -= self.overdraft_penalty
+            print("He's owes the bank ${} and doesn't get to party.".format(rich_man.overdraft_penalty))
+            return False
+        else:
+            self.balance -= dollars
+            return dollars
+    def accumulate_interest(self):
+        if(self.balance > 0):
+            self.balance *= 1.02
+            return self.balance
+        else:
+            return
+
+
+# rich_man = BankAccount("checking")
+# rich_man.deposit(100000000)
+#
+# def ballin():
+#     rich_man.withdraw(random.randint(500000, 25000000))
+#
+# for i in range(1, 40):
+#     if rich_man.overdraft_fees < 100:
+#         ballin()
+#         print("He has {} left".format(rich_man.balance))
+#     else:
+#         print("He's in ${} of debt after {} parties".format(rich_man.balance - 100, i))
+#         break
+# print("What a baller.")
+
+# rich_man = OverdraftAccount()
+# rich_man.deposit(100000000)
+#
+# def ballin():
+#     rich_man.withdraw(random.randint(1000, 100000))
+#
+# i = 0
+# while (rich_man.withdraw(random.randint(1, 50000000))):
+#     i += 1
+#     print("He has {} left".format(rich_man.balance))
+# print("He went to {} parties".format(i))
+# print("What a baller.")
+
+partner1 = BankAccount("checking")
+partner1.balance = 40000
+
+partner2 = BankAccount("checking")
+partner2.balance = 20000
+
+new_account = BankAccount("joint")
+new_account.deposit(partner1.withdraw(partner1.balance) + partner2.withdraw(partner2.balance))
+print("We have {}".format(new_account.balance))
--- week6/hw/codebar/codebar.py
+++ week6/hw/codebar/codebar.py
@@ -1,18 +1,17 @@
 class Member:
-    def __init__(self, full_name, introduction, is_student, reason, bio, skills):
+    def __init__(self, full_name, type, introduction):
         self.full_name = full_name
         self.introduction = introduction
-        self.is_student = is_student
-        self.reason = reason
+        self.type = type
+
+class Instructor(Member):
+    def __init__(self, full_name, introduction, bio, skills):
+        super().__init__(full_name, "Instructor", introduction)
         self.bio = bio
         self.skills = skills
     def addSkill(self, new_skill):
         self.skills.append(new_skill)
-    def printInsides(self):
-        amI = "student"
-        if self.is_student:
-            amI = "teacher"
-        skillset = ""
+    def print_details(self):
         if len(self.skills) > 1:
             for i, skill in enumerate(self.skills):
                 if i < len(self.skills) -1:
@@ -22,8 +21,53 @@ class Member:
         else:
             skillset = "just" + self.skills[0] + "."
 
-        print("{0} My name is {1}. I am a {2}. I'm taking this course because {3}. {4}. My skills are {5}".format(self.introduction, self.full_name, amI, self.reason, self.bio, skillset))
+        print("{0} My name is {1}. I am your instructor. {2}. My skills are {3}".format(self.introduction, self.full_name, self.bio, skillset))
 
-brienne = Member("Brienne Solis", "Hi, I guess.", True, "I want to make that $$$, yknow?", "Someday I'll be an ABG", ["statistics", "math in general"])
-brienne.addSkill("Python")
-brienne.printInsides()
+class Student(Member):
+    def __init__(self, full_name, introduction, reason):
+        super().__init__(full_name, "Student", introduction)
+        self.reason = reason
+    def print_details(self):
+        print("{0} My name is {1}. I am a student. I'm here because {2}".format(self.introduction, self.full_name, self.reason))
+
+class Workshop:
+    def __init__(self, start_date, subject):
+        self.start_date = start_date
+        self.subject = subject
+        self.instructors = []
+        self.students = []
+    def add_participant(self, participant):
+        if participant.type == "Instructor":
+            self.instructors.append(participant)
+        else:
+            self.students.append(participant)
+    def print_details(self):
+        instructor_list = ""
+        student_list = ""
+        for i, instructor in enumerate(self.instructors):
+            if i < len(self.instructors) -1:
+                instructor_list += instructor.full_name + ", "
+            else:
+                instructor_list += "and " + instructor.full_name
+        for i, student in enumerate(self.students):
+            if i < len(self.instructors) - 1:
+                student_list += student.full_name + ", "
+            else:
+                student_list += "and " + student.full_name
+        return f"Our {self.subject} class starts {self.start_date}. It is taught by {instructor_list}. {student_list} are participating"
+
+rc = Instructor("RC", "What's up?", "I'm a former DJ who is now a programmer and teacher.", ["HTML/CSS", "Javascript", "JS Frameworks"])
+luke = Instructor("Luke", "HiiiIIIIIEEEEEE!", "I'm smarter than you and it's okay.", ["data science", "programming", "math", "management"])
+
+jess = Student("Jessica Jessison", "Heyyyy!", "I'm here to change my own life.")
+coocoo = Student("Courtney Cooper", "Hey, I guess.", "I want to make money.")
+
+my_workshop = Workshop("01/10/19", "Computer Science")
+
+my_workshop.add_participant(rc)
+my_workshop.add_participant(luke)
+my_workshop.add_participant(jess)
+my_workshop.add_participant(coocoo)
+
+
+print(my_workshop.print_details())
